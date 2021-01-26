109bbca2b18e51465decff6e8c40efadb10644de
Manon <thefireforever@gmail.com>
2021-01-26 14:26:27 +0100

    ajout des classes

---
+++ src/tp2/Bloc.java
@@ -0,0 +1,64 @@
+package tp2;
+
+
+public class Bloc {
+	
+	  private String Couleur;
+
+	    private String getCouleur() {
+	        // Automatically generated method. Please do not modify this code.
+	        return this.Couleur;
+	    }
+
+	    private void setCouleur(String value) {
+	        // Automatically generated method. Please do not modify this code.
+	        this.Couleur = value;
+	    }
+
+	    private TailleBloc Taille;
+
+	    private TailleBloc getTaille() {
+	        // Automatically generated method. Please do not modify this code.
+	        return this.Taille;
+	    }
+
+	    private void setTaille(TailleBloc value) {
+	        // Automatically generated method. Please do not modify this code.
+	        this.Taille = value;
+	    }
+
+	    private Bloc dessous;
+
+	    Bloc getBloc() {
+	//begin of modifiable zone................T/f6221675-579e-4e81-896b-ea00a094f8f2
+	        // Automatically generated method. Please delete this comment before entering specific code.
+	//end of modifiable zone..................E/f6221675-579e-4e81-896b-ea00a094f8f2
+	//begin of modifiable zone................T/c0fbd1de-0193-4fde-9184-a63facd47c95
+	        return this.bloc;
+	//end of modifiable zone..................E/c0fbd1de-0193-4fde-9184-a63facd47c95
+	    }
+
+	    void setBloc(Bloc value) {
+	//begin of modifiable zone................T/a8325184-d095-4517-af5b-49cc7a0f7b2d
+	        // Automatically generated method. Please delete this comment before entering specific code.
+	        this.bloc = value;
+	//end of modifiable zone..................E/a8325184-d095-4517-af5b-49cc7a0f7b2d
+	    }
+
+	    public Bloc attraperBloc(Bloc blocSouhaite) {
+	//begin of modifiable zone(JavaCode)......C/3711638e-dcee-490b-8791-df847bcf01d6
+
+	//end of modifiable zone(JavaCode)........E/3711638e-dcee-490b-8791-df847bcf01d6
+	//begin of modifiable zone(JavaReturned)..C/3711638e-dcee-490b-8791-df847bcf01d6
+
+	//end of modifiable zone(JavaReturned)....E/3711638e-dcee-490b-8791-df847bcf01d6
+	    }
+
+	    public void lacherBloc(Bloc blocTenu) {
+	//begin of modifiable zone(JavaCode)......C/b12001cc-aa5e-4866-94e2-322dc7dcc625
+
+	//end of modifiable zone(JavaCode)........E/b12001cc-aa5e-4866-94e2-322dc7dcc625
+	    }
+
+
+}
---
+++ src/tp2/Robot.java
@@ -0,0 +1,76 @@
+package tp2;
+
+
+public class Robot {
+
+
+    public Bloc blocTenu;
+
+    private Table table;
+
+    public void CreerCube()(TailleBloc taille, String couleur) {
+//begin of modifiable zone(JavaCode)......C/7f3fe992-932b-40d4-9b20-5a250a2fb056
+
+//end of modifiable zone(JavaCode)........E/7f3fe992-932b-40d4-9b20-5a250a2fb056
+    }
+
+    public boolean tenirCube() {
+//begin of modifiable zone(JavaCode)......C/a7ca1d97-c025-4021-a510-512ee59da27e
+
+//end of modifiable zone(JavaCode)........E/a7ca1d97-c025-4021-a510-512ee59da27e
+//begin of modifiable zone(JavaReturned)..C/a7ca1d97-c025-4021-a510-512ee59da27e
+
+//end of modifiable zone(JavaReturned)....E/a7ca1d97-c025-4021-a510-512ee59da27e
+    }
+
+    public void detruireCube(Bloc blocTenu) {
+//begin of modifiable zone(JavaCode)......C/74c73c53-2797-4c1b-a581-0d38098535b7
+
+//end of modifiable zone(JavaCode)........E/74c73c53-2797-4c1b-a581-0d38098535b7
+    }
+
+    public void prendreBloc(TailleBloc taille, String couleur) {
+//begin of modifiable zone(JavaCode)......C/1122da30-fe05-4f36-9afb-f6223439c838
+
+//end of modifiable zone(JavaCode)........E/1122da30-fe05-4f36-9afb-f6223439c838
+    }
+
+    Bloc getBlocTenu() {
+//begin of modifiable zone................T/4fac5d09-b928-4cdc-bf7b-fdf5deff9ef5
+        // Automatically generated method. Please delete this comment before entering specific code.
+//end of modifiable zone..................E/4fac5d09-b928-4cdc-bf7b-fdf5deff9ef5
+//begin of modifiable zone................T/a8707c44-5761-4ebd-ad99-aa03f6dc4195
+        return this.blocTenu;
+//end of modifiable zone..................E/a8707c44-5761-4ebd-ad99-aa03f6dc4195
+    }
+
+    void setBlocTenu(Bloc value) {
+//begin of modifiable zone................T/6a762204-e4a8-4fb1-8a2d-30f4ffb7ddee
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.blocTenu = value;
+//end of modifiable zone..................E/6a762204-e4a8-4fb1-8a2d-30f4ffb7ddee
+    }
+
+    Table getTable() {
+//begin of modifiable zone................T/f33c1855-7771-466c-a8a9-bef03433ec11
+        // Automatically generated method. Please delete this comment before entering specific code.
+//end of modifiable zone..................E/f33c1855-7771-466c-a8a9-bef03433ec11
+//begin of modifiable zone................T/1698290d-0216-4123-b87d-ac562544d69f
+        return this.table;
+//end of modifiable zone..................E/1698290d-0216-4123-b87d-ac562544d69f
+    }
+
+    void setTable(Table value) {
+//begin of modifiable zone................T/433b7a10-28be-4b37-bbfb-6edb67d8bcc1
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.table = value;
+//end of modifiable zone..................E/433b7a10-28be-4b37-bbfb-6edb67d8bcc1
+    }
+
+    public void poserBloc(Bloc blocTenu) {
+//begin of modifiable zone(JavaCode)......C/54a0bb39-9894-4486-9a1b-5131777b0e4e
+
+//end of modifiable zone(JavaCode)........E/54a0bb39-9894-4486-9a1b-5131777b0e4e
+    }
+	
+}
