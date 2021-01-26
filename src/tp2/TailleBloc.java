6873f07033fcbf98d183c491704e6674c1d224d0
Manon <thefireforever@gmail.com>
2021-01-26 14:32:29 +0100

    ajout "classe" tailleBloc, taillebloc et couleur sont des enums est ce que ce sont des classes ou autre chose du coup ? ..

---
+++ src/tp2/TailleBloc.java
@@ -0,0 +1,17 @@
+package tp2;
+
+import java.io.*;
+
+public enum TailleBloc
+{
+ grand, moyen, petit;
+ 
+ public static TailleBloc getTaille(String taille) {
+		for (TailleBloc t:TailleBloc.values())
+			if (t.name().equalsIgnoreCase(taille))
+				return t;		
+		return TailleBloc.grand;
+	}
+	
+}
+
