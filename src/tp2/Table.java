b3592d5c9ddcc85b3ae719fc45563d68bcc37cca
Manon <thefireforever@gmail.com>
2021-01-26 14:38:37 +0100

    suppression commentaires de modelio

--- src/tp2/Table.java
+++ src/tp2/Table.java
@@ -9,58 +9,32 @@ public class Table {
 	 private List<Bloc> sommets = new ArrayList<Bloc> ();
 
 	    public Bloc prendreBloc(TailleBloc taille, String couleur) {
-	//begin of modifiable zone(JavaCode)......C/c0790057-54e0-4514-ad45-9fb1811bcbea
 
-	//end of modifiable zone(JavaCode)........E/c0790057-54e0-4514-ad45-9fb1811bcbea
-	//begin of modifiable zone(JavaReturned)..C/c0790057-54e0-4514-ad45-9fb1811bcbea
-
-	//end of modifiable zone(JavaReturned)....E/c0790057-54e0-4514-ad45-9fb1811bcbea
 	    }
 
 	    public boolean regardeBlocSurTable(TailleBloc taille, String couleur) {
-	//begin of modifiable zone(JavaCode)......C/7e6694c6-5b53-45ed-b017-6e26222420c1
 
-	//end of modifiable zone(JavaCode)........E/7e6694c6-5b53-45ed-b017-6e26222420c1
-	//begin of modifiable zone(JavaReturned)..C/7e6694c6-5b53-45ed-b017-6e26222420c1
-
-	//end of modifiable zone(JavaReturned)....E/7e6694c6-5b53-45ed-b017-6e26222420c1
 	    }
 
 	    public void poserBloc(Bloc blocTenu) {
-	//begin of modifiable zone(JavaCode)......C/95a49f9a-2c50-4334-8547-112eed39683b
 
-	//end of modifiable zone(JavaCode)........E/95a49f9a-2c50-4334-8547-112eed39683b
 	    }
 
 	    public boolean verifierEspace() {
-	//begin of modifiable zone(JavaCode)......C/21cb5124-b9fa-44c7-b91f-6d2e9d19dab6
 
-	//end of modifiable zone(JavaCode)........E/21cb5124-b9fa-44c7-b91f-6d2e9d19dab6
-	//begin of modifiable zone(JavaReturned)..C/21cb5124-b9fa-44c7-b91f-6d2e9d19dab6
-
-	//end of modifiable zone(JavaReturned)....E/21cb5124-b9fa-44c7-b91f-6d2e9d19dab6
 	    }
 
 	    List<Bloc> getSommets() {
-	//begin of modifiable zone................T/ac974991-0957-4c9b-82b4-640df9c465ec
-	        // Automatically generated method. Please delete this comment before entering specific code.
-	//end of modifiable zone..................E/ac974991-0957-4c9b-82b4-640df9c465ec
-	//begin of modifiable zone................T/90b10e96-1a61-4d16-a802-f428b6609b90
+
 	        return this.sommets;
-	//end of modifiable zone..................E/90b10e96-1a61-4d16-a802-f428b6609b90
 	    }
 
 	    void setSommets(List<Bloc> value) {
-	//begin of modifiable zone................T/2ae3f550-9dcf-4937-ae16-b40d9452102e
-	        // Automatically generated method. Please delete this comment before entering specific code.
 	        this.sommets = value;
-	//end of modifiable zone..................E/2ae3f550-9dcf-4937-ae16-b40d9452102e
 	    }
 
 	    public void trouveBlocPlusGrand(TailleBloc taille) {
-	//begin of modifiable zone(JavaCode)......C/aa3bfa78-8fe7-49e7-aeb4-8dd6011f770e
 
-	//end of modifiable zone(JavaCode)........E/aa3bfa78-8fe7-49e7-aeb4-8dd6011f770e
 	    }
 
 }
